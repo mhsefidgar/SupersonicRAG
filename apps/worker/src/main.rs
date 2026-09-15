9c257c7169100b2cd59f3c56921fbb9f7b3b251d
Mohammad Sefidgar <mhsefidgar@gmail.com>
2026-09-15 07:30:56 -0400

    feat: add worker executable

---
+++ apps/worker/src/main.rs
@@ -0,0 +1,13 @@
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    tracing::info!("SupersonicRAG worker started");
+    tokio::signal::ctrl_c().await.expect("failed to install ctrl-c handler");
+    tracing::info!("SupersonicRAG worker stopped");
+}
